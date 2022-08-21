0b45eae5a0b8e6c0bcae4e379adc27818de6bbff
M0osic3gg <110620677+M0osic3gg@users.noreply.github.com>
2022-08-21 11:24:33 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,5 +1,4 @@
 permission = prompt("Do you want to use JScalc? (Write 'yes' to continue)");
-while(permission === "yes") {
 first = prompt("enter your 1st num")
 first = Number(first);
 second = prompt("enter your 2nd num")
@@ -11,5 +10,3 @@ else if (operand == "*") alert(first*second)
 else if (operand == "/" || operand == "\\") alert(first/second)
 else if (operand == "%") alert(first%second)
 else alert("what are you doing in my swamp? i mean, error. try again.")
-permission = prompt("Continue calculating?")
-}
